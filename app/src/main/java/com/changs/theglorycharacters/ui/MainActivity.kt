f449ea54ab67e335bb94ec30a1911bb69d146dc8
changs97 <kin2027@gmail.com>
2023-03-27 23:23:55 +0900

    fragment_to_activity

--- app/src/main/java/com/changs/theglorycharacters/ui/MainActivity.kt
+++ app/src/main/java/com/changs/theglorycharacters/ui/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.changs.theglorycharacters.ui
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.FragmentNavigatorExtras
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.changs.theglorycharacters.R
 import com.changs.theglorycharacters.base.BaseActivity
+import com.changs.theglorycharacters.data.Character
 import com.changs.theglorycharacters.databinding.ActivityMainBinding
 import com.changs.theglorycharacters.ui.adapter.CharacterAdapter
 import com.changs.theglorycharacters.ui.custom.HorizontalMarginItemDecoration
@@ -13,12 +19,53 @@ import com.changs.theglorycharacters.viewmodel.MainViewModel
 import kotlin.math.abs
 
 
-class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
-    private val viewModel: MainViewModel by viewModels()
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main), CharacterAdapter.CharacterAdapterListener {
+    private val viewModel : MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        setPager()
+    }
+
+    private fun setPager() {
+        with(binding.homePager) {
+            adapter = CharacterAdapter(this@MainActivity)
+            offscreenPageLimit = 1
+
+            val nextItemVisiblePx = resources.getDimension(R.dimen.viewpager_next_item_visible)
+            val currentItemHorizontalMarginPx =
+                resources.getDimension(R.dimen.viewpager_current_item_horizontal_margin)
+            val pageTranslationX = nextItemVisiblePx + currentItemHorizontalMarginPx
+            val pageTransformer = ViewPager2.PageTransformer { page: View, position: Float ->
+                page.translationX = -pageTranslationX * position
+                page.scaleY = 1 - (0.25f * abs(position))
+            }
+
+
+
+            setPageTransformer(pageTransformer)
+
+            val itemDecoration = HorizontalMarginItemDecoration(
+                this@MainActivity, R.dimen.viewpager_current_item_horizontal_margin
+            )
+
+            addItemDecoration(itemDecoration)
+        }
+    }
+
+    override fun onCharacterClicked(view: View, character: Character) {
+        view.transitionName = character.id.toString()
+
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra("character", character)
+
+        val options = ActivityOptions
+            .makeSceneTransitionAnimation(this, view, character.id.toString())
+
+        startActivity(intent, options.toBundle())
 
     }
 }
--- app/src/main/java/com/changs/theglorycharacters/ui/adapter/CharacterViewHolder.kt
+++ app/src/main/java/com/changs/theglorycharacters/ui/adapter/CharacterViewHolder.kt
@@ -1,14 +1,10 @@
 package com.changs.theglorycharacters.ui.adapter
 
-import androidx.core.view.ViewCompat
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.FragmentNavigatorExtras
-import androidx.recyclerview.widget.RecyclerView
-import com.changs.theglorycharacters.R
 
+import androidx.recyclerview.widget.RecyclerView
 import com.changs.theglorycharacters.databinding.CharacterItemBinding
 import com.changs.theglorycharacters.data.Character
-import com.changs.theglorycharacters.ui.fragment.HomeFragmentDirections
+
 
 class CharacterViewHolder(
     private val binding: CharacterItemBinding, listener: CharacterAdapter.CharacterAdapterListener
