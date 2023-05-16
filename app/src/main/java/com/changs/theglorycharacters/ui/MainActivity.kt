cae9d07a69b6102b82503499d9cf5bbb145c61ee
changs97 <kin2027@gmail.com>
2023-05-16 20:24:31 +0900

    초기화 -> 지연 초기화로 변경: layoutInflater는 현재 Context를 검색해서 반환하게 되는데 현재 코드에서는 context가 생성되지 않은 시점이므로 null이다.

--- app/src/main/java/com/changs/theglorycharacters/ui/MainActivity.kt
+++ app/src/main/java/com/changs/theglorycharacters/ui/MainActivity.kt
@@ -9,7 +9,7 @@ import com.changs.theglorycharacters.ui.custom.CharacterView
 
 
 class MainActivity : AppCompatActivity() {
-    private val binding = ActivityMainBinding.inflate(layoutInflater)
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
